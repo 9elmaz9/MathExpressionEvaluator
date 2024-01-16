3a6405de457d3bfa51098daa56eb078f579d5117
Elmaz <Kakbyelmaz@gmail.com>
2024-01-16 11:52:13 +0100

    add

---
+++ src/main/java/be/intecbrussel/MathExpressionEvaluator/service/MathExpressionServiceImp.java
@@ -0,0 +1,8 @@
+package be.intecbrussel.MathExpressionEvaluator.service;
+
+public class MathExpressionServiceImp  implements MathExpressionService{
+    @Override
+    public String evaluate(String expression) {
+        return null;
+    }
+}
